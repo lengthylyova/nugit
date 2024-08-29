670fbb323717d057291c6f8ad0c218259b25df5d
Lev Zlobin <154515515+lengthylyova@users.noreply.github.com>
2024-08-30 00:04:35 +0300

    feat: quite mode for nugit job (#14)

--- src/nugit/constants.py
+++ src/nugit/constants.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass
 class NugitAbout:
     NAME: str = "nugit"
     DESCRIPTION: str = "Easy to understand, configure and install git pre-commit solution"
-    VERSION: str = "1.0.3"
+    VERSION: str = "1.0.4"
     AUTHOR: str = "Lev Zlobin (lengthylyova)"
     AUTHOR_EMAIL: str = "lengthylyova@gmail.com"
 
